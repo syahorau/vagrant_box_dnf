f96cd8e5dba506b4f68b62a37d4569d01c6f4edd
syahorau <syahorau@gmail.com>
2024-05-22 17:36:22 +0300

    separete files

--- vagrant_create_box_dnf_part_2.sh
+++ vagrant_create_box_dnf_part_2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-wget https://download.virtualbox.org/virtualbox/7.0.18/VBoxGuestAdditions_7.0.18.iso
 mkdir /virtbox 
-mount ./VBoxGuestAdditions_7.0.18.iso /virtbox && \
-sh /virtbox/VBoxLinuxAdditions.run & \
-umount /virtbox & \
+wget https://download.virtualbox.org/virtualbox/7.0.18/VBoxGuestAdditions_7.0.18.iso
+mount ./VBoxGuestAdditions_7.0.18.iso /virtbox 
+sh /virtbox/VBoxLinuxAdditions.run 
+umount /virtbox 
 rm -r /virtbox
+rm -r ./VBoxGuestAdditions_7.0.18.iso 
 sudo dd if=/dev/zero of=/EMPTY bs=1M
 sudo rm -f /EMPTY
