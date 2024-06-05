837eb1bc87a6699660c6d4b643b55875caffa51f
syahorau <syahorau@gmail.com>
2024-06-05 17:00:32 +0300

    Finished

--- part2.sh
+++ part2.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 #create folder
 mkdir /virtbox 
-wget https://download.virtualbox.org/virtualbox/7.0.18/VBoxGuestAdditions_7.0.18.iso
-mount ./VBoxGuestAdditions_7.0.18.iso /virtbox 
-sh /virtbox/VBoxLinuxAdditions.run && umount /virtbox 
-rm -r /virtbox
-rm -r ./VBoxGuestAdditions_7.0.18.iso 
-sed -i "/\/root\/part2\.sh/d" /etc/rc.d/rc.local
-chmod -x /etc/rc.d/rc.local
-rm -f /root/part2.sh
+wget https://download.virtualbox.org/virtualbox/7.0.18/VBoxGuestAdditions_7.0.18.iso && mount ./VBoxGuestAdditions_7.0.18.iso /virtbox 
+sh /virtbox/VBoxLinuxAdditions.run && umount /virtbox && rm -rf /virtbox
+rm -rf ./VBoxGuestAdditions_7.0.18.iso 
+rm -rf /etc/rc.local
+rm -ff /root/part2.sh
 sudo dd if=/dev/zero of=/EMPTY bs=1M
 sudo rm -f /EMPTY
 shutdown -h +0
