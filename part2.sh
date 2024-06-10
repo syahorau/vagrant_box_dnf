ac519367b0e200b465a040c6ce0a08bd809229b0
syahorau <syahorau@gmail.com>
2024-06-10 14:36:20 +0300

    Finished

--- part2.sh
+++ part2.sh
@@ -8,6 +8,7 @@ umount /mnt
 sleep 10s
 rm -rf ./VBoxGuestAdditions_7.0.18.iso 
 rm -rf /etc/rc.local
+rm -rf /etc/rc.d/rc.local
 rm -ff /root/part2.sh
 echo '#!/bin/bash
 /root/part3.sh' >> /etc/rc.local
--- part3.sh
+++ part3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm -rf /etc/rc.local
-rm -ff /root/part3.sh
+rm -rf /root/part3.sh
+rm -rf /etc/rc.d/rc.local
 sudo dd if=/dev/zero of=/EMPTY bs=1M
 sudo rm -f /EMPTY
 shutdown -h +0
